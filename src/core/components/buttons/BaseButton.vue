bbede36fc7d770cf992728b0fc868aef200900c7
Alfredo Filigheddu <filigheddualfredo@gmail.com>
2024-07-13 15:43:09 +0200

    fix: created base button component
    Reviewed-by: Alfredo Filigheddu

---
+++ src/core/components/buttons/BaseButton.vue
@@ -0,0 +1,42 @@
+<template>
+  <button v-if="!link" class="button" :class="[mode, color]">
+    <slot></slot>
+  </button>
+  <router-link v-else :to="to" class="button" :class="[mode, color]">
+    <slot></slot>
+  </router-link>
+</template>
+
+<script setup lang="ts">
+import { toRefs } from 'vue';
+import { bulmaColorClasses, bulmaModeClasses } from './BaseButtonUtils';
+
+const props = defineProps({
+  mode: {
+    type: String,
+    required: false,
+    default: null,
+    validator(value: string) {
+      return bulmaModeClasses.includes(value);
+    },
+  },
+  color: {
+    type: String,
+    required: false,
+    default: null,
+    validator(value: string) {
+      return bulmaColorClasses.includes(value);
+    },
+  },
+  link: {
+    type: Boolean,
+    required: false,
+    default: false,
+  },
+  to: {
+    required: false,
+    default: '/',
+  },
+});
+const { link, mode, to, color } = toRefs(props);
+</script>
---
+++ src/core/components/buttons/BaseButtonUtils.ts
@@ -0,0 +1,2 @@
+export const bulmaModeClasses: string[] = ['is-ghost', 'is-light', 'is-dark', 'is-text'];
+export const bulmaColorClasses: string[] = ['is-white', 'is-black', 'is-primary', 'is-info', 'is-success', 'is-warning', 'is--danger'];
