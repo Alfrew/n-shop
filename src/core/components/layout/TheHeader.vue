9a472b82ea93e309a7bd46b0ff52b9acd7a8c605
Alfredo Filigheddu <filigheddualfredo@gmail.com>
2024-07-23 17:26:35 +0200

    feat: theHeader component
    Reviewed-by: Alfredo Filigheddu

---
+++ src/core/components/layout/TheHeader.vue
@@ -0,0 +1,35 @@
+<template>
+  <nav class="navbar is-primary" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <router-link class="navbar-item" to="/"> FinGames </router-link>
+    </div>
+    <div class="navbar-menu">
+      <div class="navbar-end">
+        <router-link class="navbar-item" :to="{ name: 'userAuth' }">
+          <span class="icon"> <i class="fas fa-user"></i></span>
+        </router-link>
+        <!-- <router-link class="navbar-item" :to="{ name: 'cart' }">
+          <span class="icon"> <i class="fas fa-shopping-cart"></i></span>
+        </router-link> -->
+      </div>
+    </div>
+  </nav>
+</template>
+
+<style scoped lang="scss">
+.navbar,
+.navbar-end {
+  display: flex;
+  align-items: stretch;
+}
+.navbar-menu {
+  flex-grow: 1;
+  flex-shrink: 0;
+  display: flex;
+  box-shadow: none;
+}
+.navbar-end {
+  justify-content: flex-end;
+  margin-inline-start: auto;
+}
+</style>
