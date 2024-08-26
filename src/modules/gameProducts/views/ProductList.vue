625a2033c21dadc694fd0d8c60011d7d57843ef4
Alfredo Filigheddu <filigheddualfredo@gmail.com>
2024-08-26 16:43:34 +0200

    feat: added get request
    Reviewed-by: Alfredo Filigheddu

--- src/modules/gameProducts/views/ProductList.vue
+++ src/modules/gameProducts/views/ProductList.vue
@@ -1,5 +1,5 @@
 <template>
-  <ul class="grid is-col-min-8">
+  <ul class="grid is-col-min-10">
     <li v-for="productGame in productGames" :key="productGame.id" class="cell">
       <game-product-item :product-game="productGame"></game-product-item>
     </li>
@@ -7,54 +7,28 @@
 </template>
 
 <script setup lang="ts">
-import GameProductItem from '../components/GameProductItem.vue';
-import { GameProduct } from '../models/GameProduct';
+import { useStore } from "vuex";
+import GameProductItem from "../components/GameProductItem.vue";
+import { GameProduct } from "../models/GameProduct";
+import { computed, onMounted, ref } from "vue";
 
-const productGames: GameProduct[] = [
-  {
-    coverUrl: '',
-    description: 'An amazing game',
-    id: 'g1',
-    imageUrl: 'https://assets.nintendo.eu/image/private/f_auto,c_limit,w_1920,q_auto:low/t_product_tile_mobile/e61ygmjm86tdpfdupd9y',
-    name: 'Donkey Kong Country Returns HD',
-    price: 59.99,
-    type: 'GAME',
-  },
-  {
-    coverUrl: '',
-    description: 'An amazing game',
-    id: 'g1',
-    imageUrl: 'https://assets.nintendo.eu/image/private/f_auto,c_limit,w_1920,q_auto:low/t_product_tile_mobile/fmc8ivqzwhop6vi6jfjm',
-    name: 'Mario & Luigi: Fraternauti alla carica',
-    price: 59.99,
-    type: 'GAME',
-  },
-  {
-    coverUrl: '',
-    description: 'An amazing game',
-    id: 'g1',
-    imageUrl: 'https://assets.nintendo.eu/image/private/f_auto,c_limit,w_1920,q_auto:low/t_product_tile_mobile/p0s7jymrmrdofd1gwpta',
-    name: 'Super Mario Party Jamboree',
-    price: 59.99,
-    type: 'GAME',
-  },
-  {
-    coverUrl: '',
-    description: 'An amazing game',
-    id: 'g1',
-    imageUrl: 'https://assets.nintendo.eu/image/private/f_auto,c_limit,w_1920,q_auto:low/t_product_tile_mobile/fttvzefex2vv5qqgxipk',
-    name: 'The Legend of Zelda: Echoes of Wisdom + Wall Scroll Poster',
-    price: 72.99,
-    type: 'BUNDLE',
-  },
-  {
-    coverUrl: '',
-    description: 'An amazing game',
-    id: 'g1',
-    imageUrl: 'https://assets.nintendo.eu/image/private/f_auto,c_limit,w_1920,q_auto:low/t_product_tile_mobile/awkoqneedtzqxgjzf8ti',
-    name: 'The Legend of Zelda: Echoes of Wisdom',
-    price: 59.99,
-    type: 'GAME',
-  },
-];
+onMounted(() => {
+  loadProducts();
+});
+
+const store = useStore();
+const productGames = computed<GameProduct[]>(() => store.getters["gameProducts/gameProductList"]);
+const error = ref("");
+const isLoading = ref(false);
+async function loadProducts(forceRefresh = false) {
+  isLoading.value = true;
+  try {
+    await store.dispatch("gameProducts/loadGameProductList", {
+      forceRefresh: forceRefresh,
+    });
+  } catch (err: any) {
+    error.value = err.message || "Something went wrong, try again later";
+  }
+  isLoading.value = false;
+}
 </script>
