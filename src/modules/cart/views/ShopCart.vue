43668ed85ccf0b3d49e18948c7c98e30aee0f68c
Alfredo Filigheddu <filigheddualfredo@gmail.com>
2024-08-30 16:51:44 +0200

    feat: ShopCart page
    Reviewed-by: Alfredo Filigheddu

--- src/modules/cart/views/ShopCart.vue
+++ src/modules/cart/views/ShopCart.vue
@@ -1,3 +1,42 @@
 <template>
-  <div>Carrty</div>
+  <div>
+    <h1 class="title has-text-left">Shopping Cart</h1>
+    <div class="grid is-column-gap-6" v-if="cartProducts.length > 0">
+      <ul class="cell is-col-span-2">
+        <li v-if="cartTotal > 80"><base-notification color="is-success" :is-light="true">Congratulations you've qualified for free shipping!</base-notification></li>
+        <li class="cart-item" v-for="cartProduct of cartProducts" :key="cartProduct.id">
+          <cart-list-item :cart-product="cartProduct"></cart-list-item>
+        </li>
+      </ul>
+
+      <div class="cell">
+        <cart-summary></cart-summary>
+      </div>
+    </div>
+    <base-card v-else>
+      <p class="title">No item in your cart</p>
+      <p>Maybe check something <router-link :to="{ name: 'productList' }">here</router-link></p>
+    </base-card>
+  </div>
 </template>
+
+<script setup lang="ts">
+import CartListItem from "../components/CartListItem.vue";
+import CartSummary from "../components/CartSummary.vue";
+import BaseNotification from "@/core/components/elements/BaseNotification.vue";
+import { useStore } from "vuex";
+import { CartGameProduct } from "../models/CartGameProduct";
+import { computed } from "vue";
+
+const store = useStore();
+const cartProducts = computed<CartGameProduct[]>(() => store.getters["cart/cartProductList"]);
+const cartTotal = computed<number>(() => store.getters["cart/cartTotal"]);
+</script>
+
+<style scoped lang="scss">
+@import "../../../scss/variables.scss";
+
+.cart-item {
+  border-bottom: 2px solid $lightGrayColor;
+}
+</style>
