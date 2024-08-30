231c7049dff2b2bd40f9c27f4f50df59ba4cffbe
Alfredo Filigheddu <filigheddualfredo@gmail.com>
2024-08-30 16:51:03 +0200

    fix: added coverUrl in cartGameProduct interface
    Reviewed-by: Alfredo Filigheddu

--- src/modules/cart/models/CartGameProduct.ts
+++ src/modules/cart/models/CartGameProduct.ts
@@ -1,6 +1,7 @@
 import { GameProductType } from "@/modules/gameProducts/models/GameProduct";
 
 export interface CartGameProduct {
+  coverUrl: string;
   id: string;
   inStock?: number;
   name: string;
