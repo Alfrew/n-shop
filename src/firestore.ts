141ec55d3e6601a68cf11cf7aad24881fabc0454
Alfredo Filigheddu <filigheddualfredo@gmail.com>
2024-09-12 17:32:55 +0200

    feat: installed firestore
    Reviewed-by: Alfredo Filigheddu

---
+++ src/firestore.ts
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCwClQf_tZvKk51wU6AoTpqgfjEuJQ8gAk",
+  authDomain: "n-shop-b6c92.firebaseapp.com",
+  databaseURL: "https://n-shop-b6c92-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "n-shop-b6c92",
+  storageBucket: "n-shop-b6c92.appspot.com",
+  messagingSenderId: "658444567440",
+  appId: "1:658444567440:web:c59fde8db3306c0688e915",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
