a44f7e2f12bec6c8d58fcb6c1cec092eed4ec2fd
Alfredo Filigheddu <filigheddualfredo@gmail.com>
2024-09-13 15:50:25 +0200

    feat: auth store updated to firestore api
    Reviewed-by: Alfredo Filigheddu

--- src/modules/auth/store/auth.ts
+++ src/modules/auth/store/auth.ts
@@ -2,6 +2,8 @@ import { ActionContext } from "vuex";
 import { AuthStore } from "../models/AuthStore";
 import { AuthFormData } from "../models/AuthFormData";
 import router from "@/router";
+import { signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { auth } from "@/firestore";
 
 let timer: any;
 
@@ -36,6 +38,82 @@ export default {
     },
 
     async auth(context: ActionContext<AuthStore, AuthStore>, payload: AuthFormData) {
+      await signInWithEmailAndPassword(auth, payload.email, payload.password)
+        .then((useCredential: any) => {
+          const expiresIn = +useCredential._tokenResponse.expiresIn * 1000;
+          const expirationDate = new Date().getTime() + expiresIn;
+
+          const userResponse = {
+            token: useCredential._tokenResponse.idToken,
+            userId: useCredential._tokenResponse.localId,
+            tokenExpiration: expirationDate,
+          };
+          localStorage.setItem("user", JSON.stringify(userResponse));
+
+          timer = setTimeout(() => {
+            context.dispatch("autoLogout");
+          }, expiresIn);
+
+          context.commit("setUser", { ...userResponse, didAutoLogout: false });
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = new Error(error.message || "Failed to authenticate");
+          throw errorMessage;
+        });
+    },
+
+    logout(context: ActionContext<AuthStore, AuthStore>) {
+      signOut(auth)
+        .then(() => {
+          localStorage.removeItem("user");
+
+          clearTimeout(timer);
+
+          context.commit("setUser", {
+            token: null,
+            userId: null,
+            didAutoLogout: false,
+          });
+
+          router.push({ name: "userAuth" });
+        })
+        .catch((error) => {
+          // An error happened.
+        });
+    },
+
+    tryLogin(context: ActionContext<AuthStore, AuthStore>) {
+      const userJson = localStorage.getItem("user");
+      if (!userJson) return;
+
+      const user = JSON.parse(userJson);
+
+      const expiresIn = +user.tokenExpiration - new Date().getTime();
+
+      if (expiresIn < 10000) {
+        return;
+      }
+
+      timer = setTimeout(() => {
+        context.dispatch("autoLogout");
+      }, expiresIn);
+
+      if (user.token && user.userId) {
+        context.commit("setUser", { ...user, didAutoLogout: false });
+      }
+    },
+
+    autoLogout(context: ActionContext<AuthStore, AuthStore>) {
+      context.dispatch("logout");
+      context.commit("setAutoLogout");
+    },
+
+    /**
+     * Firebase realtime db to signin
+     * @deprecated Moved on firestore db, use auth instead
+     */
+    async authOLD(context: ActionContext<AuthStore, AuthStore>, payload: AuthFormData) {
       const API_KEY = "AIzaSyCwClQf_tZvKk51wU6AoTpqgfjEuJQ8gAk";
       const mode = payload.mode;
       let url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${API_KEY}`;
@@ -76,27 +154,11 @@ export default {
 
       context.commit("setUser", { ...userResponse, didAutoLogout: false });
     },
-    tryLogin(context: ActionContext<AuthStore, AuthStore>) {
-      const userJson = localStorage.getItem("user");
-      if (!userJson) return;
-
-      const user = JSON.parse(userJson);
-
-      const expiresIn = +user.tokenExpiration - new Date().getTime();
-
-      if (expiresIn < 10000) {
-        return;
-      }
-
-      timer = setTimeout(() => {
-        context.dispatch("autoLogout");
-      }, expiresIn);
-
-      if (user.token && user.userId) {
-        context.commit("setUser", { ...user, didAutoLogout: false });
-      }
-    },
-    logout(context: ActionContext<AuthStore, AuthStore>) {
+    /**
+     * Firebase realtime db to logout
+     * @deprecated Moved on firestore db, use logout instead
+     */
+    logoutOLD(context: ActionContext<AuthStore, AuthStore>) {
       localStorage.removeItem("user");
 
       clearTimeout(timer);
@@ -108,10 +170,6 @@ export default {
       });
 
       router.push({ name: "userAuth" });
-    },
-    autoLogout(context: ActionContext<AuthStore, AuthStore>) {
-      context.dispatch("logout");
-      context.commit("setAutoLogout");
     },
   },
   getters: {
