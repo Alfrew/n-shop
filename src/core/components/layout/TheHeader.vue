c90197a7bbddc34a11123769bddf049bc09ef852
Alfredo Filigheddu <filigheddualfredo@gmail.com>
2024-09-14 15:24:38 +0200

    feat: added theme toggle
    Reviewed-by: Alfredo Filigheddu

--- src/core/components/layout/TheHeader.vue
+++ src/core/components/layout/TheHeader.vue
@@ -5,18 +5,30 @@
     </div>
     <div class="navbar-menu">
       <div class="navbar-end">
-        <template v-if="isLogged">
-          <router-link class="navbar-item" title="Admin page" :to="{ name: 'adminProductList' }">
+        <transition-fade>
+          <router-link v-if="isLogged" class="navbar-item" title="Admin page" :to="{ name: 'adminProductList' }">
             <span class="icon"><i class="fas fa-list"></i></span>
           </router-link>
-          <span class="navbar-link is-arrowless" title="Logout" @click="logout()">
+        </transition-fade>
+
+        <transition-fade>
+          <span v-if="isLogged" class="navbar-link is-arrowless" title="Logout" @click="logout()">
             <span class="icon"><i class="fas fa-right-from-bracket"></i></span>
           </span>
-        </template>
 
-        <router-link v-else class="navbar-item" title="Login page" :to="{ name: 'userAuth' }">
-          <span class="icon"><i class="fas fa-user"></i></span>
-        </router-link>
+          <router-link v-else class="navbar-item" title="Login page" :to="{ name: 'userAuth' }">
+            <span class="icon"><i class="fas fa-user"></i></span>
+          </router-link>
+        </transition-fade>
+
+        <transition-fade>
+          <span v-if="isLightTheme" class="navbar-link is-arrowless" title="Enable dark theme" @click="toggleTheme()">
+            <span class="icon"><i class="fas fa-moon"></i></span>
+          </span>
+          <span v-else class="navbar-link is-arrowless" title="Enable light theme" @click="toggleTheme()">
+            <span class="icon"><i class="fas fa-sun"></i></span>
+          </span>
+        </transition-fade>
 
         <router-link class="navbar-item" title="Shopping cart" :to="{ name: 'cart' }">
           <base-badge :badge-value="cartQuantity">
@@ -29,9 +41,18 @@
 </template>
 
 <script lang="ts" setup>
-import { computed } from "vue";
-import BaseBadge from "../utilities/BaseBadge.vue";
+import { computed, ref } from "vue";
 import { useStore } from "vuex";
+import BaseBadge from "../utilities/BaseBadge.vue";
+import TransitionFade from "../animations/TransitionFade.vue";
+
+const theme = ref<"light" | "dark">("light");
+const isLightTheme = computed<boolean>(() => theme.value == "light");
+document.querySelector("html")!.setAttribute("data-theme", theme.value);
+function toggleTheme() {
+  theme.value = isLightTheme.value ? "dark" : "light";
+  document.querySelector("html")!.setAttribute("data-theme", theme.value);
+}
 
 const store = useStore();
 const isLogged = computed<boolean>(() => store.getters["isAuthenticated"]);
