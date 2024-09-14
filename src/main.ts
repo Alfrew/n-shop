e2a30880c157528872d4d229966edfb49bb380b4
Alfredo Filigheddu <filigheddualfredo@gmail.com>
2024-09-14 13:34:18 +0200

    feat: BasePageTitle component
    Reviewed-by: Alfredo Filigheddu

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,10 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-import BaseButton from "./core/components/buttons/BaseButton.vue";
-import BaseSpinner from "./core/components/utilities/BaseSpinner.vue";
 import BaseBox from "./core/components/containers/BaseBox.vue";
+import BaseButton from "./core/components/buttons/BaseButton.vue";
+import BasePageTitle from "./core/components/elements/BasePageTitle.vue";
+import BaseSpinner from "./core/components/utilities/BaseSpinner.vue";
 
 require("./scss/main.scss");
 
@@ -16,9 +17,10 @@ const app = createApp(App);
 app.use(store);
 app.use(router);
 
-app.component("base-button", BaseButton);
 app.component("base-box", BaseBox);
+app.component("base-button", BaseButton);
 app.component("base-dialog", BaseDialog);
+app.component("base-page-title", BasePageTitle);
 app.component("base-spinner", BaseSpinner);
 
 app.mount("#app");
